f8a32aa2b232c7cc3bcd20fc5eb82e23c869343a
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-26 22:16:16 +0100

    Commit

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Supplier;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,20 @@ class SupplierSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $suppliers = [
+            [
+                'name' => "Demo Supplier",
+                'phone' => "0101010000",
+                'address' => "Demo Address",
+            ],
+        ];
+
+        foreach ($suppliers as $item) {
+            $supplier = new Supplier();
+            $supplier->name = $item['name'];
+            $supplier->phone = $item['phone'];
+            $supplier->address = $item['address'];
+            $supplier->save();
+        }
     }
 }
