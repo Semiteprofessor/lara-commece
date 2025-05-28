7c61e682a81a55cda0a4ea276ef1104c2a3c4d1f
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-28 22:05:13 +0100

    Commit

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Department;
 
 class DepartmentSeeder extends Seeder
 {
@@ -12,6 +13,9 @@ class DepartmentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $department = new Department();
+        $department->name = 'Demo Department';
+        $department->save();
+
     }
 }
