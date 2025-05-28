a2e9e2e917ddde8f85e2454c04c5994e932be653
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-28 22:10:46 +0100

    Commit

--- database/seeders/ProductBrandSeeder.php
+++ database/seeders/ProductBrandSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ProductBrand;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,18 @@ class ProductBrandSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $brands = [
+            "Brand 1",
+            "Brand 2",
+            "Brand 3",
+            "Brand 4",
+            "Brand 5",
+        ];
+
+        foreach ($brands as $brand) {
+            $productBrand = new ProductBrand();
+            $productBrand->name = $brand;
+            $productBrand->save();
+        }
     }
 }
