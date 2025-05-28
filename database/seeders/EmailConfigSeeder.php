27f549fe7092c607c4982f1ed0723ba8851ab84b
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-28 22:08:02 +0100

    Commit

--- database/seeders/EmailConfigSeeder.php
+++ database/seeders/EmailConfigSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\EmailConfig;
 
 class EmailConfigSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class EmailConfigSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $emailConfig = new EmailConfig();
+        $emailConfig->emailConfigName = 'InventoryOs';
+        $emailConfig->emailHost = 'mail.lfix.us';
+        $emailConfig->emailPort = '465';
+        $emailConfig->emailUser = 'no-reply@lfix.us';
+        $emailConfig->emailPass = '@omega@2020@omega';
+        $emailConfig->save();
     }
 }
