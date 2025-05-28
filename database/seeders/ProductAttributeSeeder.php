5fd13e9a878a0ec978e51f1334948092e1095c3c
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-28 22:10:06 +0100

    Commit

--- database/seeders/ProductAttributeSeeder.php
+++ database/seeders/ProductAttributeSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\ProductAttribute;
 use Illuminate\Database\Seeder;
 
 class ProductAttributeSeeder extends Seeder
@@ -12,6 +13,9 @@ class ProductAttributeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $productAttribute = new ProductAttribute();
+        $productAttribute->name = 'Demo Attribute';
+        $productAttribute->save();
+
     }
 }
