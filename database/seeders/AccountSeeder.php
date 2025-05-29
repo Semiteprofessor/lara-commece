c13dff5754a8a7a35abb7e198fd88bf00f1d207e
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-29 21:04:47 +0100

    Commit

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Account;
 
 class AccountSeeder extends Seeder
 {
@@ -12,6 +13,34 @@ class AccountSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $account = new Account();
+        $account->name = 'Asset';
+        $account->type = 'Asset';
+        $account->save();
+
+        $account = new Account();
+        $account->name = 'Liability';
+        $account->type = 'Liability';
+        $account->save();
+
+        $account = new Account();
+        $account->name = 'Equity';
+        $account->type = 'Equity';
+        $account->save();
+
+        $account = new Account();
+        $account->name = 'Withdrawal';
+        $account->type = 'Equity';
+        $account->save();
+
+        $account = new Account();
+        $account->name = 'Revenue';
+        $account->type = 'Equity';
+        $account->save();
+
+        $account = new Account();
+        $account->name = 'Expense';
+        $account->type = 'Equity';
+        $account->save();
     }
 }
