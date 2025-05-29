60394d5f4e036719f207cc6b80a1e4e6843ba535
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-29 21:11:48 +0100

    Commit

--- database/seeders/ColorsSeeder.php
+++ database/seeders/ColorsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Colors;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,19 @@ class ColorsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $colors = [
+            [
+                'name' => "Black",
+                'colorCode' => "#000000",
+            ]
+
+        ];
+
+        foreach ($colors as $item) {
+            $color = new Colors();
+            $color->name = $item['name'];
+            $color->colorCode = $item['colorCode'];
+            $color->save();
+        }
     }
 }
