5e79c99fa2f097011bab5a5305f6e9663550f652
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-29 21:57:15 +0100

    Commit

--- database/seeders/CustomerPermissionsSeeder.php
+++ database/seeders/CustomerPermissionsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\CustomerPermissions;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,33 @@ class CustomerPermissionsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $customerEndpoints = [
+            'product_details',
+            'profile',
+            'purchase',
+        ];
+
+        $endpointPermissionMapping = [
+            'product_details' => ["read"],
+            'profile' => ["read", "update"],
+            'purchase' => ["create", "read", "update"],
+        ];
+
+        $customerPermissionsData = [];
+
+        foreach ($customerEndpoints as $endpoint) {
+            $permissionTypes = $endpointPermissionMapping[$endpoint];
+            foreach ($permissionTypes as $permissionType) {
+                $permission = "{$permissionType}-{$endpoint}";
+                $customerPermissionsData[] = $permission;
+            }
+        }
+
+        foreach ($customerPermissionsData as $permission) {
+            $customerPermissions = new CustomerPermissions();
+            $customerPermissions->user = 'customer';
+            $customerPermissions->permissions = $permission;
+            $customerPermissions->save();
+        }
     }
 }
