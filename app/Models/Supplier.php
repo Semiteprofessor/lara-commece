851d11b4b2a26994517c3c907b0a288436e834f1
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-31 18:34:16 +0100

    Commit

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -4,9 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Supplier extends Model
 {
     /** @use HasFactory<\Database\Factories\SupplierFactory> */
     use HasFactory;
+
+    protected $table = 'supplier';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+        'phone',
+        'address',
+        'email',
+    ];
+
+    public function purchaseInvoice(): HasMany
+    {
+        return $this->hasMany(PurchaseInvoice::class, 'supplierId');
+    }
 }
