ee7b2f10deb9c39cf6753016a8f2f23c1dc2b407
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-31 18:31:30 +0100

    Commit

--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -4,9 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Shift extends Model
 {
     /** @use HasFactory<\Database\Factories\ShiftFactory> */
     use HasFactory;
+
+    //create shift model
+    protected $table = 'shift';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+        'startTime',
+        'endTime',
+        'workHour',
+    ];
+
+    public function user(): HasMany
+    {
+        return $this->hasMany(User::class, "shiftId");
+    }
 }
