bf893325027576fc5164de9d1b0850c6c23ad0b2
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-31 18:30:35 +0100

    Commit

--- app/Models/SaleInvoiceVat.php
+++ app/Models/SaleInvoiceVat.php
@@ -4,9 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SaleInvoiceVat extends Model
 {
     /** @use HasFactory<\Database\Factories\SaleInvoiceVatFactory> */
     use HasFactory;
+    protected $table = 'saleInvoiceVat';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'invoiceId',
+        'productVatId',
+    ];
+
+    // public function saleInvoice(): BelongsTo
+    // {
+    //     return $this->belongsTo(SaleInvoice::class, 'invoiceId');
+    // }
+
+    public function productVat(): BelongsTo
+    {
+        return $this->belongsTo(ProductVat::class, 'productVatId');
+    }
 }
