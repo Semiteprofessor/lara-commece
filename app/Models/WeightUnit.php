e5ad1fdcf9f53dde0ffb1142c0167b9832623eee
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-31 18:37:14 +0100

    Commit

--- app/Models/WeightUnit.php
+++ app/Models/WeightUnit.php
@@ -9,4 +9,10 @@ class WeightUnit extends Model
 {
     /** @use HasFactory<\Database\Factories\WeightUnitFactory> */
     use HasFactory;
+
+    protected $table = 'weightUnit';
+
+    protected $fillable = [
+        'name'
+    ];
 }
--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -4,9 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Wishlist extends Model
 {
     /** @use HasFactory<\Database\Factories\WishlistFactory> */
     use HasFactory;
+
+    public $table = 'wishlist';
+
+    protected $fillable = [
+        'customerId',
+    ];
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class, 'customerId');
+    }
+
+    public function productWishlist(): HasMany
+    {
+        return $this->hasMany(ProductWishlist::class, 'wishlistId');
+    }
 }
