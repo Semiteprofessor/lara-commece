503d2988d2f651736effe95a64baa041c8636d74
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-01 18:26:51 +0100

    Commit

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
     /** @use HasFactory<\Database\Factories\RoleFactory> */
     use HasFactory;
+
+    //create role model
+    protected $table = 'role';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+    ];
+    public function rolePermission(): HasMany
+    {
+        return $this->hasMany(RolePermission::class, 'roleId');
+    }
 }
