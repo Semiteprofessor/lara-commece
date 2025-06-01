c48db59903606fbba8c67e768ab39dbccdd19b9e
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-01 18:27:42 +0100

    Commit

--- app/Models/RolePermission.php
+++ app/Models/RolePermission.php
@@ -4,9 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RolePermission extends Model
 {
     /** @use HasFactory<\Database\Factories\RolePermissionFactory> */
     use HasFactory;
+    protected $table = 'rolePermission';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'roleId',
+        'permissionId',
+    ];
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class, 'roleId');
+    }
+    public function permission(): BelongsTo
+    {
+        return $this->belongsTo(Permission::class, 'permissionId');
+    }
 }
