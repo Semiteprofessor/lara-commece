415d1267629d9958249eeda25be4286d67804dcc
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-01 18:25:58 +0100

    Commit

--- app/Models/ReviewReply.php
+++ app/Models/ReviewReply.php
@@ -4,9 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ReviewReply extends Model
 {
     /** @use HasFactory<\Database\Factories\ReviewReplyFactory> */
     use HasFactory;
+
+    protected $table = 'reviewReply';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'reviewId',
+        'adminId',
+        'comment',
+    ];
+
+    public function review(): BelongsTo
+    {
+        return $this->belongsTo(ReviewRating::class, 'reviewId');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'adminId');
+    }
 }
