81da9ae30488e6a4659c9d99d41bcbd79f77315c
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-01 18:28:16 +0100

    Commit

--- app/Models/SalaryHistory.php
+++ app/Models/SalaryHistory.php
@@ -4,9 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SalaryHistory extends Model
 {
     /** @use HasFactory<\Database\Factories\SalaryHistoryFactory> */
     use HasFactory;
+
+    protected $table = 'salaryHistory';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'userId',
+        'salary',
+        'startDate',
+        'endDate',
+        'comment',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
 }
