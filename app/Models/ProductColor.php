65bee6ad9780605747f0b6c6f46b3b12c28c5aa3
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-02 17:34:29 +0100

    Commit

--- app/Models/ProductColor.php
+++ app/Models/ProductColor.php
@@ -4,9 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductColor extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductColorFactory> */
     use HasFactory;
+    protected $table = 'productColor';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'productId',
+        'colorId',
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'productId');
+    }
+
+    public function color(): BelongsTo
+    {
+        return $this->belongsTo(Colors::class, 'colorId');
+    }
 }
