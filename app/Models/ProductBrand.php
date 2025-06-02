942ef525fd6cb5bd0f531725f13ae7a3ed49602d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-02 17:33:03 +0100

    Commit

--- app/Models/ProductBrand.php
+++ app/Models/ProductBrand.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ProductBrand extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductBrandFactory> */
     use HasFactory;
+
+    protected $table = 'productBrand';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+    ];
+
+    public function product(): HasMany
+    {
+        return $this->hasMany(Product::class, 'productBrandId');
+    }
 }
