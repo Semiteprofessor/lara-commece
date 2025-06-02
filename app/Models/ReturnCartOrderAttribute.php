d31e7414b5511bb48dd4dbbe7658e43719158900
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-02 17:56:26 +0100

    Commit

--- app/Models/ReturnCartOrderAttribute.php
+++ app/Models/ReturnCartOrderAttribute.php
@@ -4,9 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ReturnCartOrderAttribute extends Model
 {
     /** @use HasFactory<\Database\Factories\ReturnCartOrderAttributeFactory> */
     use HasFactory;
+    protected $table = 'returnCartOrderAttributeValue';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'returnCartOrderProductId',
+        'productAttributeValueId',
+    ];
+
+    public function returnCartOrderProduct(): BelongsTo
+    {
+        return $this->belongsTo(ReturnCartOrderProduct::class, 'returnCartOrderProductId');
+    }
+
+    public function productAttributeValue(): BelongsTo
+    {
+        return $this->belongsTo(ProductAttributeValue::class, 'productAttributeValueId');
+    }
 }
