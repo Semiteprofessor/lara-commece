a4bc76a9c5462882fd565bd691faa57d445ca60d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-02 17:32:20 +0100

    Commit

--- app/Models/ProductAttributeValue.php
+++ app/Models/ProductAttributeValue.php
@@ -9,4 +9,25 @@ class ProductAttributeValue extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductAttributeValueFactory> */
     use HasFactory;
+    protected $table = 'productAttributeValue';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'productAttributeId',
+        'name',
+    ];
+
+    public function productAttribute()
+    {
+        return $this->belongsTo(ProductAttribute::class, 'productAttributeId');
+    }
+
+    public function productProductAttributeValue()
+    {
+        return $this->hasMany(ProductProductAttributeValue::class, 'productAttributeValueId');
+    }
+
+    public function cartProduct()
+    {
+        return $this->hasMany(CartProduct::class, 'productAttributeValueId');
+    }
 }
