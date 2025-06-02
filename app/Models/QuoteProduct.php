6331a541d84696ba3c343515c0fcccfb6aeaea8f
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-02 17:49:59 +0100

    Commit

--- app/Models/QuoteProduct.php
+++ app/Models/QuoteProduct.php
@@ -4,9 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class QuoteProduct extends Model
 {
     /** @use HasFactory<\Database\Factories\QuoteProductFactory> */
     use HasFactory;
+    protected $table = 'quoteProduct';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'quoteId',
+        'productId',
+        'unitPrice',
+        'productQuantity',
+    ];
+
+    public function quote(): BelongsTo
+    {
+        return $this->belongsTo(Quote::class, 'quoteId');
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'productId');
+    }
 }
