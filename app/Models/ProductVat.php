92e6d8ade605ef489b1354034093244d97f74820
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-02 17:36:59 +0100

    Commit

--- app/Models/ProductVat.php
+++ app/Models/ProductVat.php
@@ -9,4 +9,21 @@ class ProductVat extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductVatFactory> */
     use HasFactory;
+
+    public $table = 'productWishlist';
+
+    protected $fillable = [
+        'wishlistId',
+        'productId',
+    ];
+
+    public function wishlist()
+    {
+        return $this->belongsTo(Wishlist::class, 'wishlistId');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'productId');
+    }
 }
