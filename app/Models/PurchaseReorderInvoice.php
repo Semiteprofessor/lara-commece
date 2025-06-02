5defe5e2ea85a5abefae226746da56261d179409
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-02 17:44:43 +0100

    Commit

--- app/Models/PurchaseReorderInvoice.php
+++ app/Models/PurchaseReorderInvoice.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PurchaseReorderInvoice extends Model
 {
     /** @use HasFactory<\Database\Factories\PurchaseReorderInvoiceFactory> */
     use HasFactory;
+    protected $table = 'purchaseReorderInvoice';
+    protected $fillable = [
+        'reorderInvoiceId',
+        'productId',
+        'productQuantity',
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'productId');
+    }
 }
