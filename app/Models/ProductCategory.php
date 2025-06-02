73ec3a7e49bd63a4feb8b353ed546fa87ba90f0c
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-02 17:33:59 +0100

    Commit

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ProductCategory extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductCategoryFactory> */
     use HasFactory;
+    protected $table = 'productCategory';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+    ];
+
+    public function productSubCategory(): HasMany
+    {
+        return $this->hasMany(ProductSubCategory::class, 'productCategoryId');
+    }
 }
