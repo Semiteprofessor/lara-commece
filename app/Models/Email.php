6d487d69378253d2d896b840c8e51b74edd17f3d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-03 17:23:24 +0100

    Commit

--- app/Models/Email.php
+++ app/Models/Email.php
@@ -9,4 +9,31 @@ class Email extends Model
 {
     /** @use HasFactory<\Database\Factories\EmailFactory> */
     use HasFactory;
+
+    protected $table = 'email';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'senderEmail',
+        'receiverEmail',
+        'subject',
+        'body',
+        'emailStatus',
+    ];
+
+
+    public function cc()
+    {
+        return $this->hasMany(Cc::class, 'emailId');
+    }
+
+    public function bcc()
+    {
+        return $this->hasMany(Bcc::class, 'emailId');
+    }
+
+    public function attachment()
+    {
+        return $this->hasMany(Attachment::class, 'emailId');
+    }
 }
