5fe45c9334b1db6987b7ba89017a508471a85f85
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-03 17:18:38 +0100

    Commit

--- app/Models/DesignationHistory.php
+++ app/Models/DesignationHistory.php
@@ -4,9 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DesignationHistory extends Model
 {
     /** @use HasFactory<\Database\Factories\DesignationHistoryFactory> */
     use HasFactory;
+    protected $table = 'designationHistory';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'userId',
+        'designationId',
+        'startDate',
+        'endDate',
+        'comment',
+    ];
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+
+    public function designation(): BelongsTo
+    {
+        return $this->belongsTo(Designation::class, 'designationId');
+    }
 }
