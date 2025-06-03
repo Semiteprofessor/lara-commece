a4896d6853f8219d853172a73bf5e78d590840ed
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-03 17:15:50 +0100

    Commit

--- app/Models/DeliveryChallanProduct.php
+++ app/Models/DeliveryChallanProduct.php
@@ -4,9 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DeliveryChallanProduct extends Model
 {
     /** @use HasFactory<\Database\Factories\DeliveryChallanProductFactory> */
     use HasFactory;
+
+    protected $table = 'deliveryChallanProduct';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'deliveryChallanId',
+        'productId',
+        'quantity'
+    ];
+
+    public function deliveryChallan(): BelongsTo
+    {
+        return $this->belongsTo(DeliveryChallan::class, 'deliveryChallanId', 'challanNo');
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'productId', 'id');
+    }
 }
