7be140ab23ad5d798ec631628ecbba2e6325aa10
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-03 17:17:26 +0100

    Commit

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -4,9 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Department extends Model
 {
     /** @use HasFactory<\Database\Factories\DepartmentFactory> */
     use HasFactory;
+
+    //create department model
+    protected $table = 'department';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+    ];
+
+    public function user(): HasMany
+    {
+        return $this->hasMany(User::class, 'departmentId');
+    }
 }
