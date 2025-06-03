aa1378924ef8876cffdb2bdee650bb6509aba193
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-03 17:11:18 +0100

    Commit

--- app/Models/Cc.php
+++ app/Models/Cc.php
@@ -4,9 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cc extends Model
 {
     /** @use HasFactory<\Database\Factories\CcFactory> */
     use HasFactory;
+
+    protected $table = 'cc';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'emailId',
+        'crmEmailId',
+        'ccEmail',
+        'status',
+    ];
+
+    public function email(): BelongsTo
+    {
+        return $this->belongsTo(Email::class, 'emailId', 'id');
+    }
 }
