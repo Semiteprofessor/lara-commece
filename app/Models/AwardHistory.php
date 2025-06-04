79b1e4609868f379fc21236bb18295c4d95b5b63
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-04 16:56:25 +0100

    Commit

--- app/Models/AwardHistory.php
+++ app/Models/AwardHistory.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AwardHistory extends Model
 {
@@ -22,7 +23,7 @@ class AwardHistory extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(Users::class, 'userId');
+        return $this->belongsTo(User::class, 'userId');
     }
 
     public function award(): BelongsTo
