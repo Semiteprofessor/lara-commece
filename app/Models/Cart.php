310d743ad1a830a464abdf57784d0181dd3b6baa
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-04 16:57:33 +0100

    Commit

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,9 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cart extends Model
 {
     /** @use HasFactory<\Database\Factories\CartFactory> */
     use HasFactory;
+
+    protected $table = 'cart';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'customerId',
+        'totalAmount'
+    ];
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class, 'customerId');
+    }
+
+    public function cartProducts(): HasMany
+    {
+        return $this->hasMany(CartProduct::class, 'cartId', 'id');
+    }
 }
