9d41d0f85f92f01451f506fe7000a043c44a59f0
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-04 16:55:11 +0100

    Commit

--- app/Models/AwardHistory.php
+++ app/Models/AwardHistory.php
@@ -9,4 +9,24 @@ class AwardHistory extends Model
 {
     /** @use HasFactory<\Database\Factories\AwardHistoryFactory> */
     use HasFactory;
+
+    protected $table = 'awardHistory';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'userId',
+        'awardId',
+        'awardedDate',
+        'comment',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(Users::class, 'userId');
+    }
+
+    public function award(): BelongsTo
+    {
+        return $this->belongsTo(Award::class, 'awardId');
+    }
 }
