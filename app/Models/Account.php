359ea48b37cf425b9ea573dfd5f0522b7259d357
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-04 12:39:52 +0100

    Commit

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Account extends Model
 {
@@ -16,7 +17,7 @@ class Account extends Model
         'name','type',
     ];
 
-    public function SubAccount()
+    public function SubAccount(): HasMany
     {
         return $this->hasMany(SubAccount::class, 'accountId');
     }
--- app/Models/AdjustInvoice.php
+++ app/Models/AdjustInvoice.php
@@ -4,9 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AdjustInvoice extends Model
 {
     /** @use HasFactory<\Database\Factories\AdjustInvoiceFactory> */
     use HasFactory;
+
+    protected $table = 'adjustInvoice';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'adjustType','note', 'date','userId',
+    ];
+
+    public function adjustInvoiceProduct(): HasMany
+    {
+        return $this->hasMany(AdjustInvoiceProduct::class,'adjustInvoiceId','id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'userId', 'id');
+    }
 }
