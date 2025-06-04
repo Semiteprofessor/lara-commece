22a332d51c487630235d8c51b72918a07d0fa8f6
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-04 12:31:28 +0100

    Commit

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -9,4 +9,15 @@ class Account extends Model
 {
     /** @use HasFactory<\Database\Factories\AccountFactory> */
     use HasFactory;
+
+    protected $table = 'account';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name','type',
+    ];
+
+    public function SubAccount()
+    {
+        return $this->hasMany(SubAccount::class, 'accountId');
+    }
 }
