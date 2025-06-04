9df8bb67248208ac9c0b22acf947d075d33a76fb
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-04 16:59:09 +0100

    Commit

--- app/Models/CartAttributeValue.php
+++ app/Models/CartAttributeValue.php
@@ -4,9 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CartAttributeValue extends Model
 {
     /** @use HasFactory<\Database\Factories\CartAttributeValueFactory> */
     use HasFactory;
+
+    protected $table = 'cartAttributeValue';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'cartProductId',
+        'productAttributeValueId',
+    ];
+
+    public function cartProduct(): BelongsTo
+    {
+        return $this->belongsTo(CartProduct::class, 'cartProductId', 'id');
+    }
+
+    public function productAttributeValue(): BelongsTo
+    {
+        return $this->belongsTo(ProductAttributeValue::class, 'productAttributeValueId', 'id');
+    }
 }
