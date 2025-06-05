6395f7cef3f44b4c331d6a2ed6c73edcfe485dc6
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-05 11:53:09 +0100

    Commit

--- database/migrations/2025_06_13_213333_create_return_cart_order_products_table.php
+++ database/migrations/2025_06_13_213333_create_return_cart_order_products_table.php
@@ -11,9 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('return_cart_order_products', function (Blueprint $table) {
+        Schema::create('returnCartOrderProduct', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('productId');
+            $table->unsignedBigInteger('cartOrderProductId');
+            $table->unsignedBigInteger('returnCartOrderId');
+            $table->unsignedBigInteger('colorId')->nullable();
+            $table->integer('productQuantity');
+            $table->double('productSalePrice');
+            $table->integer('productVat')->nullable();
+            $table->string('discountType')->nullable();
+            $table->integer('discount')->nullable();
             $table->timestamps();
+
+            $table->foreign('productId')->references('id')->on('product');
+            $table->foreign('cartOrderProductId')->references('id')->on('cartOrderProduct')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('returnCartOrderId')->references('id')->on('returnCartOrder')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('colorId')->references('id')->on('colors');
         });
     }
 
@@ -22,6 +36,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('return_cart_order_products');
+        Schema::dropIfExists('returnCartOrderProduct');
     }
 };
