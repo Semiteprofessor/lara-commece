0803c6c1c0b7f5db4aa39881b70739f691b8f522
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-05 12:03:12 +0100

    Commit

--- database/migrations/2025_06_13_211633_create_manufacturers_table.php
+++ database/migrations/2025_06_13_211633_create_manufacturers_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('manufacturers', function (Blueprint $table) {
+        Schema::create('manufacturer', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('status')->default('true');
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('manufacturers');
+        Schema::dropIfExists('manufacturer');
     }
 };
