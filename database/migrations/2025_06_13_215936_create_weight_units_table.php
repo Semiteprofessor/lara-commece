67a9f4e0f63c0252e23a329d14f988309c78e3d0
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-05 12:04:52 +0100

    Commit

--- database/migrations/2025_06_13_215936_create_weight_units_table.php
+++ database/migrations/2025_06_13_215936_create_weight_units_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('weight_units', function (Blueprint $table) {
+        Schema::create('weightUnit', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('status')->default('true');
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('weight_units');
+        Schema::dropIfExists('weightUnit');
     }
 };
