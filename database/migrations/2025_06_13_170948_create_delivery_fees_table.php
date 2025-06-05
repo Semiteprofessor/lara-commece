0f568839daa6217791ff208f18f862950ce486a5
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-05 11:57:20 +0100

    Commit

--- database/migrations/2025_06_13_170948_create_delivery_fees_table.php
+++ database/migrations/2025_06_13_170948_create_delivery_fees_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('delivery_fees', function (Blueprint $table) {
+        Schema::create('deliveryFee', function (Blueprint $table) {
             $table->id();
+            $table->string('deliveryArea');
+            $table->double('deliveryFee');
+            $table->string('status')->default('true');
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('delivery_fees');
+        Schema::dropIfExists('deliveryFee');
     }
 };
