5a8d4c840e729d832d2df54a77e034988e8f7bc0
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-07 08:57:53 +0100

    Commit

--- database/migrations/2025_06_13_211227_create_ecom_settings_table.php
+++ database/migrations/2025_06_13_211227_create_ecom_settings_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('ecom_settings', function (Blueprint $table) {
+        Schema::create('ecom_setting', function (Blueprint $table) {
             $table->id();
+            $table->string('IsActive')->default('true');
             $table->timestamps();
         });
     }
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('ecom_settings');
+        Schema::dropIfExists('ecom_setting');
     }
 };
