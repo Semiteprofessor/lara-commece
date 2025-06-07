01c6162deddc1d06e23def4312060c1bb31f892d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-07 09:25:51 +0100

    Commit

--- database/migrations/2025_06_13_212348_create_product_attributes_table.php
+++ database/migrations/2025_06_13_212348_create_product_attributes_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('product_attributes', function (Blueprint $table) {
+        Schema::create('productAttribute', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('status')->default('true');
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('product_attributes');
+        Schema::dropIfExists('productAttribute');
     }
 };
