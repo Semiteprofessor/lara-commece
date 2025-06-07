832a8ed82bfd976a0a41877853663faaec434095
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-07 11:43:02 +0100

    Commit

--- database/migrations/2025_06_13_170050_create_cart_orders_table.php
+++ database/migrations/2025_06_13_170050_create_cart_orders_table.php
@@ -11,9 +11,31 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cart_orders', function (Blueprint $table) {
-            $table->id();
+        Schema::create('cartOrder', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->dateTime('date');
+            $table->double('totalAmount');
+            $table->double('paidAmount')->nullable();
+            $table->double('deliveryFee')->nullable();
+            $table->double('due')->nullable();
+            $table->string('isPaid')->default('false');
+            $table->double('profit');
+            $table->unsignedBigInteger('couponId')->nullable();
+            $table->double('couponAmount')->nullable();
+            $table->unsignedBigInteger('customerId');
+            $table->unsignedBigInteger('userId');
+            $table->string('deliveryAddress')->nullable();
+            $table->string('customerPhone')->nullable();
+            $table->string('note')->nullable();
+            $table->string('isReOrdered')->default('false');
+            $table->enum('orderStatus', ['PENDING', 'RECEIVED', 'SHIPPED', 'DELIVERED', 'RETURNED', 'CANCELLED'])->default('PENDING');
+            $table->string('status')->default('true');
             $table->timestamps();
+
+            // foreign key
+            $table->foreign('customerId')->references('id')->on('customer')->onDelete('cascade');
+            $table->foreign('userId')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('couponId')->references('id')->on('coupon');
         });
     }
 
@@ -22,6 +44,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cart_orders');
+        Schema::dropIfExists('cartOrder');
     }
 };
