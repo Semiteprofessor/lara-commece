2a8501ad13a235880bb012ad497fefe4247f5147
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-08 22:59:11 +0100

    Commit

---
+++ app/Http/Controllers/UoMController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UoM;
+use App\Http\Requests\StoreUoMRequest;
+use App\Http\Requests\UpdateUoMRequest;
+
+class UoMController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUoMRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UoM $uoM)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUoMRequest $request, UoM $uoM)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UoM $uoM)
+    {
+        //
+    }
+}
