1a9c1e6a144068a848140c255823e1be837218f5
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-08 20:08:45 +0100

    Commit

--- database/migrations/2025_06_13_165730_create_carts_table.php
+++ database/migrations/2025_06_13_165730_create_carts_table.php
@@ -11,9 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('carts', function (Blueprint $table) {
+        Schema::create('cart', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('customerId');
+            $table->double('totalAmount');
             $table->timestamps();
+
+            $table->foreign('customerId')->references('id')->on('customer');
         });
     }
 
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('carts');
+        Schema::dropIfExists('cart');
     }
 };
