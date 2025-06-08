9d2b6ff5ac9c2036581158596a91dd00c540c3d4
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-08 23:05:45 +0100

    Commit

--- database/migrations/2025_06_13_171004_create_departments_table.php
+++ database/migrations/2025_06_13_171004_create_departments_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('departments', function (Blueprint $table) {
+        Schema::create('department', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('status')->default("true");
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('departments');
+        Schema::dropIfExists('department');
     }
 };
