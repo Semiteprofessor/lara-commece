4d80152a2cc51016cbfdf6de11f7b103141742f0
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-08 19:53:57 +0100

    Commit

--- database/migrations/2025_06_13_165412_create_adjust_invoice_products_table.php
+++ database/migrations/2025_06_13_165412_create_adjust_invoice_products_table.php
@@ -11,9 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('adjust_invoice_products', function (Blueprint $table) {
+        Schema::create('adjustInvoiceProduct', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('adjustInvoiceId');
+            $table->unsignedBigInteger('productId')->nullable();
+            $table->integer('adjustQuantity');
+            $table->string('adjustType')->nullable();
             $table->timestamps();
+
+            $table->foreign('adjustInvoiceId')->references('id')->on('adjustInvoice');
+            $table->foreign('productId')->references('id')->on('product');
         });
     }
 
@@ -22,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('adjust_invoice_products');
+        Schema::dropIfExists('adjustInvoiceProduct');
     }
 };
