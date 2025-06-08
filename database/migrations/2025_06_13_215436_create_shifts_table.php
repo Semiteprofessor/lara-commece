3e3e1ee84d094c754630c03b0a46ec7f74c080e3
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-08 23:07:06 +0100

    Commit

--- database/migrations/2025_06_13_215436_create_shifts_table.php
+++ database/migrations/2025_06_13_215436_create_shifts_table.php
@@ -11,8 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('shifts', function (Blueprint $table) {
+        Schema::create('shift', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->time('startTime');
+            $table->time('endTime');
+            $table->double('workHour')->nullable();
+            $table->string('status')->default("true");
             $table->timestamps();
         });
     }
@@ -22,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('shifts');
+        Schema::dropIfExists('shift');
     }
 };
