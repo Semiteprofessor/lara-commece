ba3647765728dfba3473453291b137b8a3881f7f
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-08 20:08:15 +0100

    Commit

--- database/migrations/2025_06_13_170843_create_customer_permissions_table.php
+++ database/migrations/2025_06_13_170843_create_customer_permissions_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('customer_permissions', function (Blueprint $table) {
+        Schema::create('customerPermissions', function (Blueprint $table) {
             $table->id();
+            $table->string('user');
+            $table->string('permissions');
+            $table->string('status')->default('true');
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('customer_permissions');
+        Schema::dropIfExists('customerPermissions');
     }
 };
