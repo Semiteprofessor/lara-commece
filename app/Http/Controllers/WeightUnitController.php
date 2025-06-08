63ec85197a2eec6b78debc1dd05aa050803c4993
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-08 22:59:41 +0100

    Commit

---
+++ app/Http/Controllers/WeightUnitController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WeightUnit;
+use App\Http\Requests\StoreWeightUnitRequest;
+use App\Http\Requests\UpdateWeightUnitRequest;
+
+class WeightUnitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreWeightUnitRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(WeightUnit $weightUnit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateWeightUnitRequest $request, WeightUnit $weightUnit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(WeightUnit $weightUnit)
+    {
+        //
+    }
+}
