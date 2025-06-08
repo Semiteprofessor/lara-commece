dcb34efbe83b0ed7ad87d07ebc2f4bff5ef67dbf
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-08 23:04:48 +0100

    Commit

--- database/migrations/2025_06_13_211513_create_employment_statuses_table.php
+++ database/migrations/2025_06_13_211513_create_employment_statuses_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('employment_statuses', function (Blueprint $table) {
+        Schema::create('employmentStatus', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('colourValue');
+            $table->string('description')->nullable();
+            $table->string('status')->default("true");
             $table->timestamps();
         });
     }
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('employment_statuses');
+        Schema::dropIfExists('employmentStatus');
     }
 };
