7e02da307847cac255a2569f7475fa9ef1346723
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-09 22:56:34 +0100

    Commit

---
+++ app/Http/Controllers/SMSPurchaseController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SMSPurchase;
+use App\Http\Requests\StoreSMSPurchaseRequest;
+use App\Http\Requests\UpdateSMSPurchaseRequest;
+
+class SMSPurchaseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSMSPurchaseRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SMSPurchase $sMSPurchase)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSMSPurchaseRequest $request, SMSPurchase $sMSPurchase)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SMSPurchase $sMSPurchase)
+    {
+        //
+    }
+}
