7c0e908598001e9c13e329353fbbb688b7f272e9
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-09 22:55:39 +0100

    Commit

---
+++ app/Http/Controllers/ShippingTimeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ShippingTime;
+use App\Http\Requests\StoreShippingTimeRequest;
+use App\Http\Requests\UpdateShippingTimeRequest;
+
+class ShippingTimeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreShippingTimeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ShippingTime $shippingTime)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateShippingTimeRequest $request, ShippingTime $shippingTime)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ShippingTime $shippingTime)
+    {
+        //
+    }
+}
---
+++ database/seeders/ShippingTimeSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ShippingTimeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
