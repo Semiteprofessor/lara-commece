d99b517d32ec9b3c10aee265040fb91bd2024351
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-09 22:56:08 +0100

    Commit

---
+++ app/Http/Controllers/SliderImagesController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SliderImages;
+use App\Http\Requests\StoreSliderImagesRequest;
+use App\Http\Requests\UpdateSliderImagesRequest;
+
+class SliderImagesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSliderImagesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SliderImages $sliderImages)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSliderImagesRequest $request, SliderImages $sliderImages)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SliderImages $sliderImages)
+    {
+        //
+    }
+}
