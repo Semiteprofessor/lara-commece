d925c2c3bcb947b1a14ea5bb2b71d2c6b4a4a92b
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-09 22:55:20 +0100

    Commit

---
+++ app/Http/Controllers/ShippingChargeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ShippingCharge;
+use App\Http\Requests\StoreShippingChargeRequest;
+use App\Http\Requests\UpdateShippingChargeRequest;
+
+class ShippingChargeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreShippingChargeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ShippingCharge $shippingCharge)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateShippingChargeRequest $request, ShippingCharge $shippingCharge)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ShippingCharge $shippingCharge)
+    {
+        //
+    }
+}
