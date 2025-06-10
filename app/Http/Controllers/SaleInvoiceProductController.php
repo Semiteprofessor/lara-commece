ae7c690323725d8e16eb61362181b48ac8612973
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-10 22:53:40 +0100

    Commit

---
+++ app/Http/Controllers/SaleInvoiceProductController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SaleInvoiceProduct;
+use App\Http\Requests\StoreSaleInvoiceProductRequest;
+use App\Http\Requests\UpdateSaleInvoiceProductRequest;
+
+class SaleInvoiceProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSaleInvoiceProductRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SaleInvoiceProduct $saleInvoiceProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSaleInvoiceProductRequest $request, SaleInvoiceProduct $saleInvoiceProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SaleInvoiceProduct $saleInvoiceProduct)
+    {
+        //
+    }
+}
