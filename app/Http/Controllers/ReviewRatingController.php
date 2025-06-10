d49366e7821fc832d0bf6d1e14626b70dd4c301c
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-10 22:37:12 +0100

    Commit

---
+++ app/Http/Controllers/ReviewRatingController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ReviewRating;
+use App\Http\Requests\StoreReviewRatingRequest;
+use App\Http\Requests\UpdateReviewRatingRequest;
+
+class ReviewRatingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreReviewRatingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ReviewRating $reviewRating)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReviewRatingRequest $request, ReviewRating $reviewRating)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ReviewRating $reviewRating)
+    {
+        //
+    }
+}
