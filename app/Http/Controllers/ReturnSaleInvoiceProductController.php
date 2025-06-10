d40eed5d4d53729b3db24d005eda10030b29ed69
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-10 22:36:47 +0100

    Commit

---
+++ app/Http/Controllers/ReturnSaleInvoiceProductController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ReturnSaleInvoiceProduct;
+use App\Http\Requests\StoreReturnSaleInvoiceProductRequest;
+use App\Http\Requests\UpdateReturnSaleInvoiceProductRequest;
+
+class ReturnSaleInvoiceProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreReturnSaleInvoiceProductRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ReturnSaleInvoiceProduct $returnSaleInvoiceProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReturnSaleInvoiceProductRequest $request, ReturnSaleInvoiceProduct $returnSaleInvoiceProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ReturnSaleInvoiceProduct $returnSaleInvoiceProduct)
+    {
+        //
+    }
+}
---
+++ database/seeders/ReturnSaleInvoiceProductSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ReturnSaleInvoiceProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
