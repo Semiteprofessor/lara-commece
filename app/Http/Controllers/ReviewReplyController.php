e4c5ef9f61aad64eca707cef6cd784b5defcb4d0
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-10 22:37:44 +0100

    Commit

---
+++ app/Http/Controllers/ReviewReplyController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ReviewReply;
+use App\Http\Requests\StoreReviewReplyRequest;
+use App\Http\Requests\UpdateReviewReplyRequest;
+
+class ReviewReplyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreReviewReplyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ReviewReply $reviewReply)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReviewReplyRequest $request, ReviewReply $reviewReply)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ReviewReply $reviewReply)
+    {
+        //
+    }
+}
