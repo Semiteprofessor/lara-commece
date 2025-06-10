ed909a29273e4b1c0caf082429dbd0bede3930d0
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-10 22:36:32 +0100

    Commit

---
+++ app/Http/Controllers/ReturnSaleInvoiceController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ReturnSaleInvoice;
+use App\Http\Requests\StoreReturnSaleInvoiceRequest;
+use App\Http\Requests\UpdateReturnSaleInvoiceRequest;
+
+class ReturnSaleInvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreReturnSaleInvoiceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ReturnSaleInvoice $returnSaleInvoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReturnSaleInvoiceRequest $request, ReturnSaleInvoice $returnSaleInvoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ReturnSaleInvoice $returnSaleInvoice)
+    {
+        //
+    }
+}
