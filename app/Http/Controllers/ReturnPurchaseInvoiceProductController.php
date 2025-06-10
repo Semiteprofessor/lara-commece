79e83a7804ea41cd03b30a0ceac9d89561f28f9f
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-10 22:35:46 +0100

    Commit

---
+++ app/Http/Controllers/ReturnPurchaseInvoiceProductController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ReturnPurchaseInvoiceProduct;
+use App\Http\Requests\StoreReturnPurchaseInvoiceProductRequest;
+use App\Http\Requests\UpdateReturnPurchaseInvoiceProductRequest;
+
+class ReturnPurchaseInvoiceProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreReturnPurchaseInvoiceProductRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ReturnPurchaseInvoiceProduct $returnPurchaseInvoiceProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReturnPurchaseInvoiceProductRequest $request, ReturnPurchaseInvoiceProduct $returnPurchaseInvoiceProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ReturnPurchaseInvoiceProduct $returnPurchaseInvoiceProduct)
+    {
+        //
+    }
+}
---
+++ app/Models/ReturnPurchaseInvoiceProduct.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReturnPurchaseInvoiceProduct extends Model
+{
+    /** @use HasFactory<\Database\Factories\ReturnPurchaseInvoiceProductFactory> */
+    use HasFactory;
+}
