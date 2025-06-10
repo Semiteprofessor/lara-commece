29f29100d2d800f6f796f2ea00d86d136915cef5
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-10 22:54:15 +0100

    Commit

---
+++ app/Http/Controllers/SaleInvoiceVatController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SaleInvoiceVat;
+use App\Http\Requests\StoreSaleInvoiceVatRequest;
+use App\Http\Requests\UpdateSaleInvoiceVatRequest;
+
+class SaleInvoiceVatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSaleInvoiceVatRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SaleInvoiceVat $saleInvoiceVat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSaleInvoiceVatRequest $request, SaleInvoiceVat $saleInvoiceVat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SaleInvoiceVat $saleInvoiceVat)
+    {
+        //
+    }
+}
---
+++ app/Models/SaleInvoiceVat.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SaleInvoiceVat extends Model
+{
+    /** @use HasFactory<\Database\Factories\SaleInvoiceVatFactory> */
+    use HasFactory;
+}
