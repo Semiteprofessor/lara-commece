c7292c76a3ab349f6f3feff31d07b0188ea1cedc
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-11 22:24:28 +0100

    Commit

---
+++ app/Http/Controllers/ProductAttributeValueController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductAttributeValue;
+use App\Http\Requests\StoreProductAttributeValueRequest;
+use App\Http\Requests\UpdateProductAttributeValueRequest;
+
+class ProductAttributeValueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductAttributeValueRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductAttributeValue $productAttributeValue)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductAttributeValueRequest $request, ProductAttributeValue $productAttributeValue)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductAttributeValue $productAttributeValue)
+    {
+        //
+    }
+}
