24daca7e5053bccb823216b320f686b31d2ecafb
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-11 22:33:04 +0100

    Commit

---
+++ app/Http/Controllers/ReturnCartOrderAttributeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ReturnCartOrderAttribute;
+use App\Http\Requests\StoreReturnCartOrderAttributeRequest;
+use App\Http\Requests\UpdateReturnCartOrderAttributeRequest;
+
+class ReturnCartOrderAttributeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreReturnCartOrderAttributeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ReturnCartOrderAttribute $returnCartOrderAttribute)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReturnCartOrderAttributeRequest $request, ReturnCartOrderAttribute $returnCartOrderAttribute)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ReturnCartOrderAttribute $returnCartOrderAttribute)
+    {
+        //
+    }
+}
