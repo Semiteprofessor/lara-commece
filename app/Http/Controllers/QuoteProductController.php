cc6ea8d167f6f5549f65be4da3a2db48088b263c
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-11 22:32:05 +0100

    Commit

---
+++ app/Http/Controllers/QuoteProductController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\QuoteProduct;
+use App\Http\Requests\StoreQuoteProductRequest;
+use App\Http\Requests\UpdateQuoteProductRequest;
+
+class QuoteProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreQuoteProductRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(QuoteProduct $quoteProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateQuoteProductRequest $request, QuoteProduct $quoteProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(QuoteProduct $quoteProduct)
+    {
+        //
+    }
+}
