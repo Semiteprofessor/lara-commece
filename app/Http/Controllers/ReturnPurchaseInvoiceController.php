c6b905b2c0a0cfe447b8076207fd0d7f05399363
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-11 22:34:26 +0100

    Commit

---
+++ app/Http/Controllers/ReturnPurchaseInvoiceController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ReturnPurchaseInvoice;
+use App\Http\Requests\StoreReturnPurchaseInvoiceRequest;
+use App\Http\Requests\UpdateReturnPurchaseInvoiceRequest;
+
+class ReturnPurchaseInvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreReturnPurchaseInvoiceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ReturnPurchaseInvoice $returnPurchaseInvoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReturnPurchaseInvoiceRequest $request, ReturnPurchaseInvoice $returnPurchaseInvoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ReturnPurchaseInvoice $returnPurchaseInvoice)
+    {
+        //
+    }
+}
