10e936a08ab7d17a721116138fe752e7ecfd7568
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-11 22:26:40 +0100

    Commit

---
+++ app/Http/Controllers/ProductSubCategoryController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductSubCategory;
+use App\Http\Requests\StoreProductSubCategoryRequest;
+use App\Http\Requests\UpdateProductSubCategoryRequest;
+
+class ProductSubCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductSubCategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductSubCategoryRequest $request, ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+}
