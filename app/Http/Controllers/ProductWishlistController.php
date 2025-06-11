ec68326a0408e709152963e8bc7b0f9091dfa83d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-11 22:28:19 +0100

    Commit

---
+++ app/Http/Controllers/ProductWishlistController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductWishlist;
+use App\Http\Requests\StoreProductWishlistRequest;
+use App\Http\Requests\UpdateProductWishlistRequest;
+
+class ProductWishlistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductWishlistRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductWishlist $productWishlist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductWishlistRequest $request, ProductWishlist $productWishlist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductWishlist $productWishlist)
+    {
+        //
+    }
+}
