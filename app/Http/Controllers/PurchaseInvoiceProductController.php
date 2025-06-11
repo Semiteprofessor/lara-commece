05b3a0e96b4a350b2d011a17ff60467eeeb8655d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-11 22:29:39 +0100

    Commit

---
+++ app/Http/Controllers/PurchaseInvoiceProductController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PurchaseInvoiceProduct;
+use App\Http\Requests\StorePurchaseInvoiceProductRequest;
+use App\Http\Requests\UpdatePurchaseInvoiceProductRequest;
+
+class PurchaseInvoiceProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePurchaseInvoiceProductRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PurchaseInvoiceProduct $purchaseInvoiceProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePurchaseInvoiceProductRequest $request, PurchaseInvoiceProduct $purchaseInvoiceProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PurchaseInvoiceProduct $purchaseInvoiceProduct)
+    {
+        //
+    }
+}
