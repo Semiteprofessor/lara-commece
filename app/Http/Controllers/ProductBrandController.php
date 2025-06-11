7148c11696540c99e155a2241a157aa01a6545cd
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-11 22:24:49 +0100

    Commit

---
+++ app/Http/Controllers/ProductBrandController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductBrand;
+use App\Http\Requests\StoreProductBrandRequest;
+use App\Http\Requests\UpdateProductBrandRequest;
+
+class ProductBrandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductBrandRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductBrand $productBrand)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductBrandRequest $request, ProductBrand $productBrand)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductBrand $productBrand)
+    {
+        //
+    }
+}
