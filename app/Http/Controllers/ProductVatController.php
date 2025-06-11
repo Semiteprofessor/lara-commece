b27577180d6b6864da16a284f1ff0c4443da97a4
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-11 22:27:51 +0100

    Commit

---
+++ app/Http/Controllers/ProductVatController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductVat;
+use App\Http\Requests\StoreProductVatRequest;
+use App\Http\Requests\UpdateProductVatRequest;
+
+class ProductVatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductVatRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductVat $productVat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductVatRequest $request, ProductVat $productVat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductVat $productVat)
+    {
+        //
+    }
+}
