c177771beabe7b9aeabbe09ce56c2a3783e59c43
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-12 22:13:00 +0100

    Commit

---
+++ app/Http/Controllers/EcomSettingController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EcomSetting;
+use App\Http\Requests\StoreEcomSettingRequest;
+use App\Http\Requests\UpdateEcomSettingRequest;
+
+class EcomSettingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEcomSettingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EcomSetting $ecomSetting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEcomSettingRequest $request, EcomSetting $ecomSetting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EcomSetting $ecomSetting)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/EsucationController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Esucation;
+use App\Http\Requests\StoreEsucationRequest;
+use App\Http\Requests\UpdateEsucationRequest;
+
+class EsucationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEsucationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Esucation $esucation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEsucationRequest $request, Esucation $esucation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Esucation $esucation)
+    {
+        //
+    }
+}
