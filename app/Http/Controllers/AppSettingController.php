84402e97648c043f9d2304cc6bc94c344811e7a8
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-13 17:56:38 +0100

    Commit

---
+++ app/Http/Controllers/AppSettingController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AppSetting;
+use App\Http\Requests\StoreAppSettingRequest;
+use App\Http\Requests\UpdateAppSettingRequest;
+
+class AppSettingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAppSettingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AppSetting $appSetting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAppSettingRequest $request, AppSetting $appSetting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AppSetting $appSetting)
+    {
+        //
+    }
+}
