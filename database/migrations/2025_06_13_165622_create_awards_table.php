77bb0cfe03fd6eebcd31d299b2213ce909c15699
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-14 18:16:59 +0100

    Commit

--- database/migrations/2025_06_13_165622_create_awards_table.php
+++ database/migrations/2025_06_13_165622_create_awards_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('awards', function (Blueprint $table) {
+        Schema::create('award', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('status')->default("true");
             $table->timestamps();
         });
     }
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('awards');
+        Schema::dropIfExists('award');
     }
 };
