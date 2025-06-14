5652375b17461ea6bf9748a2fddfcf2a10804df2
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-14 18:10:15 +0100

    Commit

--- database/migrations/2025_06_13_215124_create_role_permissions_table.php
+++ database/migrations/2025_06_13_215124_create_role_permissions_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('role_permissions', function (Blueprint $table) {
+        Schema::create('rolePermission', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('roleId');
+            $table->unsignedBigInteger('permissionId');
+            $table->foreign('roleId')->references('id')->on('role');
+            $table->foreign('permissionId')->references('id')->on('permission');
             $table->timestamps();
         });
     }
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('role_permissions');
+        Schema::dropIfExists('rolePermission');
     }
 };
