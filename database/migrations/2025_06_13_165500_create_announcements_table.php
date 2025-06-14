6a3b30b509806e5cdfadccc283f2a5cc2e06c46f
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-14 18:16:08 +0100

    Commit

--- database/migrations/2025_06_13_165500_create_announcements_table.php
+++ database/migrations/2025_06_13_165500_create_announcements_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('announcements', function (Blueprint $table) {
+        Schema::create('announcement', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('status')->default("true");
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('announcements');
+        Schema::dropIfExists('announcement');
     }
 };
