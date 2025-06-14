5b7aa131f2f0b01cb7e8d13762d7b583d048a479
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-14 18:09:39 +0100

    Commit

--- database/migrations/2025_06_13_212222_create_permissions_table.php
+++ database/migrations/2025_06_13_212222_create_permissions_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('permissions', function (Blueprint $table) {
+        Schema::create('permission', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('permissions');
+        Schema::dropIfExists('permission');
     }
 };
