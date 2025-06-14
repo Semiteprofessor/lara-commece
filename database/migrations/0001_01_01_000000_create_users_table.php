2a47345085b6685ec057a438601eb4e8a1334acf
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-14 18:07:38 +0100

    Commit

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,27 +13,36 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('username')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('refreshToken')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('employeeId')->unique();
+            $table->string('phone')->nullable();
+            $table->string('street')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('zipCode')->nullable();
+            $table->string('country')->nullable();
+            $table->string('bloodGroup')->nullable();
+            $table->string('image')->nullable();
+            $table->unsignedBigInteger('employmentStatusId')->nullable();
+            $table->unsignedBigInteger('departmentId')->nullable();
+            $table->unsignedBigInteger('shiftId')->nullable();
+            $table->unsignedBigInteger('roleId');
+            $table->dateTime('joinDate')->nullable();
+            $table->dateTime('leaveDate')->nullable();
+            $table->string('isLogin')->default('false');
+            $table->string('status')->default('true');
             $table->timestamps();
-        });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+            // Foreign key constraints
+            $table->foreign('roleId')->references('id')->on('role');
+            $table->foreign('employmentStatusId')->references('id')->on('employmentStatus');
+            $table->foreign('departmentId')->references('id')->on('department');
+            $table->foreign('shiftId')->references('id')->on('shift');
         });
     }
 
@@ -43,7 +52,5 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
     }
 };
