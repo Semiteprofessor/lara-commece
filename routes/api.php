61ee64f505b62c8d538ac008b3ac27652d9b7e91
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-17 13:49:48 +0100

    Commit

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\AdjustInventoryController;
 use App\Http\Controllers\RefreshTokenController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
@@ -24,18 +25,20 @@ use Illuminate\Support\Facades\Route;
     Route::middleware('permission:update-transaction')->put('/{id}', [AccountController::class, 'updateSubAccount']);
     Route::middleware('permission:delete-transaction')->patch('/{id}', [AccountController::class, 'deleteSubAccount']);
 
+    Route::post('/login', [UsersController::class, 'login']);
+    Route::post('/logout', [UsersController::class, 'logout']);
+    Route::post('/register', [UsersController::class, 'register']);
+    //refresh token routes
+    Route::get('/refresh-token', [RefreshTokenController::class, 'validationRefreshToken']);
 
-Route::post('/login', [UsersController::class, 'login']);
-Route::post('/logout', [UsersController::class, 'logout']);
-Route::post('/register', [UsersController::class, 'register']);
-//refresh token routes
-Route::get('/refresh-token', [RefreshTokenController::class, 'validationRefreshToken']);
+    Route::middleware("permission:readAll-user")->get('/', [UsersController::class, 'getAllUser']);
+    Route::middleware("permission:readSingle-user")->get('/{id}', [UsersController::class, 'getSingleUser']);
+    Route::middleware("permission:update-user")->put("/{id}", [UsersController::class, 'updateSingleUser']);
+    Route::middleware("permission:delete-user")->patch("/{id}", [UsersController::class, 'deleteUser']);
 
-Route::middleware("permission:readAll-user")->get('/', [UsersController::class, 'getAllUser']);
-Route::middleware("permission:readSingle-user")->get('/{id}', [UsersController::class, 'getSingleUser']);
-Route::middleware("permission:update-user")->put("/{id}", [UsersController::class, 'updateSingleUser']);
-Route::middleware("permission:delete-user")->patch("/{id}", [UsersController::class, 'deleteUser']);
-
+    Route::middleware('permission:create-adjust')->post("/", [AdjustInventoryController::class, 'createAdjustInventory']);
+    Route::middleware('permission:readAll-adjust')->get("/", [AdjustInventoryController::class, 'getAllAdjustInvoices']);
+    Route::middleware('permission:readSingle-adjust')->get("/{id}", [AdjustInventoryController::class, 'getSingleAdjustInvoice']);
 
 
 //});
