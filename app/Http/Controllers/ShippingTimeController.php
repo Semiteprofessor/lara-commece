568f07492655347d074682c3564eb4523aa833f5
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-17 11:38:59 +0100

    Commit

--- app/Http/Controllers/ShippingTimeController.php
+++ app/Http/Controllers/ShippingTimeController.php
@@ -2,49 +2,144 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Models\ShippingTime;
-use App\Http\Requests\StoreShippingTimeRequest;
-use App\Http\Requests\UpdateShippingTimeRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class ShippingTimeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    //
+
+    public function createSingleShippingTime(Request $request): JsonResponse
     {
-        //
+        if ($request->query('query') === 'createmany') {
+            try {
+                $shippingTimeData = $request->json()->all();
+                $shippingTime = collect($shippingTimeData)->map(function ($item) {
+                    $shipping = ShippingTime::where('Destination', $item['Destination'])->first();
+                    if ($shipping) {
+                        return null;
+                    }
+                    return $item;
+                })->filter(function ($item) {
+                    return $item !== null;
+                })->toArray();
+
+                //if all shippingCharge already exists
+                if (count($shippingTime) === 0) {
+                    return response()->json(['error' => 'All products already exists.'], 500);
+                }
+
+                $createdShippingTime = collect($shippingTime)->map(function ($item) {
+                    return ShippingTime::firstOrCreate($item);
+                });
+
+                $result = [
+                    'count' => count($createdShippingTime),
+                ];
+
+                return response()->json($result, 201);
+            } catch (Exception $err) {
+                return response()->json(['error' => 'An error occurred during creating many ShippingTime. Please try again later.'], 500);
+            }
+        } else if ($request->query('query') === 'deletemany') {
+            try {
+
+                $data = json_decode($request->getContent(), true);
+                $deletedShippingTime = ShippingTime::destroy($data);
+
+                $deletedCounted = [
+                    'count' => $deletedShippingTime,
+                ];
+                return response()->json($deletedCounted, 200);
+            } catch (Exception $err) {
+                return response()->json(['error' => 'An error occurred during deleting many ShippingTime. Please try again later.'], 500);
+            }
+        } else {
+            try {
+                $createdShippingTime = ShippingTime::create([
+                    'Destination' => $request->input('Destination'),
+                    'EstimatedTimeDays' => $request->input('EstimatedTimeDays'),
+                ]);
+
+                $converted = arrayKeysToCamelCase($createdShippingTime->toArray());
+                return response()->json($converted, 201);
+            } catch (Exception $err) {
+                return response()->json(['error' => 'An error occurred during creating ShippingTime. Please try again later.'], 500);
+            }
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreShippingTimeRequest $request)
+    public function getAllShippingTime(Request $request): JsonResponse
     {
-        //
+        if ($request->query('query') === 'all') {
+            try {
+                $shippingTime = ShippingTime::where('status', 'true')->orderBy('id', 'desc')->get();
+                $converted = arrayKeysToCamelCase($shippingTime->toArray());
+                return response()->json($converted, 200);
+            } catch (Exception $err) {
+                return response()->json(['error' => 'An error occurred during getting all ShippingTime. Please try again later.'], 500);
+            }
+        } else {
+            try {
+                $pagination = getPagination($request->query());
+                $shippingTime = ShippingTime::where('status', $request->query('status'))
+                    ->orderBy('id', 'desc')
+                    ->skip($pagination['skip'])
+                    ->take($pagination['limit'])
+                    ->get();
+                $converted = arrayKeysToCamelCase($shippingTime->toArray());
+                return response()->json($converted, 200);
+            } catch (Exception $err) {
+                return response()->json(['error' => 'An error occurred during getting all ShippingTime. Please try again later.'], 500);
+            }
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(ShippingTime $shippingTime)
+    public function getSingleShippingTime($id): JsonResponse
     {
-        //
+        try {
+            $shippingTime = ShippingTime::with('product')->findOrFail($id);
+            $converted = arrayKeysToCamelCase($shippingTime->toArray());
+            return response()->json($converted, 200);
+        } catch (Exception $err) {
+            echo $err;
+            return response()->json(['error' => 'An error occurred during getting a single ShippingTime. Please try again later.'], 500);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateShippingTimeRequest $request, ShippingTime $shippingTime)
+    public function updateSingleShippingTime(Request $request, $id): JsonResponse
     {
-        //
+        try {
+            $shippingTime = ShippingTime::findOrFail($id);
+            $shippingTime->update([
+                'Destination' => $request->input('Destination') ?? $shippingTime->Destination,
+                'EstimatedTimeDays' => $request->input('EstimatedTimeDays') ?? $shippingTime->EstimatedTimeDays,
+            ]);
+
+            $converted = arrayKeysToCamelCase($shippingTime->toArray());
+            return response()->json($converted, 200);
+        } catch (Exception $err) {
+            return response()->json(['error' => 'An error occurred during updating a single ShippingTime. Please try again later.'], 500);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(ShippingTime $shippingTime)
+    public function deleteSingleShippingTime(Request $request, $id): JsonResponse
     {
-        //
+        try {
+            $shippingTime = ShippingTime::where('id', (int)$id)
+                ->update([
+                    'status' => $request->input('status'),
+                ]);
+
+            if (!$shippingTime) {
+                return response()->json(['error' => 'ShippingTime not found.'], 404);
+            }
+
+            return response()->json(['success' => 'ShippingTime deleted successfully.'], 200);
+        } catch (Exception $err) {
+            return response()->json(['error' => 'An error occurred during deleting a single ShippingTime. Please try again later.'], 500);
+        }
     }
 }
