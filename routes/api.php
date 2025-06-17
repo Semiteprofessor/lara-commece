ac61793157b2701b805b751ca3e9dd0c00195391
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-17 13:44:06 +0100

    Commit

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-
+use App\Http\Controllers\AccountController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\AccountController;
+
 
 /*
 |--------------------------------------------------------------------------
