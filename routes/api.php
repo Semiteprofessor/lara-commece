9366d6e43b6dca9f6a12b2d82e88aec979f3cb16
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-17 13:47:45 +0100

    Commit

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\RefreshTokenController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +23,19 @@ use Illuminate\Support\Facades\Route;
     Route::middleware('permission:readSingle-transaction')->get('/{id}', [AccountController::class, 'getSingleAccount']);
     Route::middleware('permission:update-transaction')->put('/{id}', [AccountController::class, 'updateSubAccount']);
     Route::middleware('permission:delete-transaction')->patch('/{id}', [AccountController::class, 'deleteSubAccount']);
+
+
+Route::post('/login', [UsersController::class, 'login']);
+Route::post('/logout', [UsersController::class, 'logout']);
+Route::post('/register', [UsersController::class, 'register']);
+//refresh token routes
+Route::get('/refresh-token', [RefreshTokenController::class, 'validationRefreshToken']);
+
+Route::middleware("permission:readAll-user")->get('/', [UsersController::class, 'getAllUser']);
+Route::middleware("permission:readSingle-user")->get('/{id}', [UsersController::class, 'getSingleUser']);
+Route::middleware("permission:update-user")->put("/{id}", [UsersController::class, 'updateSingleUser']);
+Route::middleware("permission:delete-user")->patch("/{id}", [UsersController::class, 'deleteUser']);
+
+
+
 //});
