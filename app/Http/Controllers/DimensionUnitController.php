03df575d80ff52057cdbcf48619c375375fcdd1d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-19 10:19:21 +0100

    Commit

--- app/Http/Controllers/DimensionUnitController.php
+++ app/Http/Controllers/DimensionUnitController.php
@@ -2,49 +2,69 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use Illuminate\Http\Request;
 use App\Models\DimensionUnit;
-use App\Http\Requests\StoreDimensionUnitRequest;
-use App\Http\Requests\UpdateDimensionUnitRequest;
 
 class DimensionUnitController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function createSingleDimensionUnit(Request $request)
     {
-        //
+        try{
+            $dimensionUnit = DimensionUnit::where('name', $request->name)->first();
+            if($dimensionUnit){
+                return response()->json(['error' => 'A Dimension Unit with the same name already exists'], 400);
+            }
+            $dimensionUnit = new DimensionUnit();
+            $dimensionUnit->name = $request->name;
+            $dimensionUnit->save();
+            $converted = arrayKeysToCamelCase($dimensionUnit->toArray());
+            return response()->json($converted, 201);
+        }catch(Exception $e){
+            echo $e;
+            return response()->json(['error' => 'An error occurred while creating the Dimension Unit record'], 500);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreDimensionUnitRequest $request)
+
+    public function getAllDimensionUnit()
     {
-        //
+        try{
+            $dimensionUnits = DimensionUnit::where('status', 'true')->get();
+            $converted = arrayKeysToCamelCase($dimensionUnits->toArray());
+            return response()->json($converted, 200);
+        }catch(Exception $e){
+            return response()->json(['error' => 'An error occurred while retrieving the Dimension Units'], 500);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(DimensionUnit $dimensionUnit)
+    public function updateSingleDimensionUnit(Request $request, $id)
     {
-        //
+        try{
+            $dimensionUnit = DimensionUnit::where('name', $request->name)->first();
+            if($dimensionUnit){
+                return response()->json(['error' => 'A Dimension Unit with the same name already exists'], 400);
+            }
+            $dimensionUnit = DimensionUnit::find($id);
+            $dimensionUnit->name = $request->name;
+            $dimensionUnit->save();
+            $converted = arrayKeysToCamelCase($dimensionUnit->toArray());
+            return response()->json($converted, 200);
+        }catch(Exception $e){
+            return response()->json(['error' => 'An error occurred while updating the Dimension Unit record'], 500);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateDimensionUnitRequest $request, DimensionUnit $dimensionUnit)
+    public function deleteSingleDimensionUnit(Request $request, $id)
     {
-        //
+        try{
+            $dimensionUnit = DimensionUnit::find($id);
+            $dimensionUnit->status = $request->status;
+            $dimensionUnit->save();
+            return response()->json(['message' => 'Dimension Unit record deleted successfully'], 200);
+        } catch(Exception $e){
+            return response()->json(['error' => 'An error occurred while deleting the Dimension Unit record'], 500);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(DimensionUnit $dimensionUnit)
-    {
-        //
-    }
 }
