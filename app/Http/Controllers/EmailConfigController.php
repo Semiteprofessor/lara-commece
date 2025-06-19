9b7e7b53280936da73379ace6313104b305fadec
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-19 10:21:57 +0100

    Commit

--- app/Http/Controllers/EmailConfigController.php
+++ app/Http/Controllers/EmailConfigController.php
@@ -1,50 +1,91 @@
 <?php
 
 namespace App\Http\Controllers;
-
+//
 use App\Models\EmailConfig;
-use App\Http\Requests\StoreEmailConfigRequest;
-use App\Http\Requests\UpdateEmailConfigRequest;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class EmailConfigController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    //create email config
+    public function createEmailConfig(Request $request): JsonResponse
     {
-        //
+        try {
+            $emailConfig = EmailConfig::create([
+                'emailConfigName' => $request->input("emailConfigName"),
+                'emailHost' => $request->input("emailHost"),
+                'emailPort' => $request->input("emailPort"),
+                'emailUser' => $request->input("emailUser"),
+                'emailPass' => $request->input("emailPass"),
+            ]);
+
+            $converted = arrayKeysToCamelCase($emailConfig->toArray());
+            return response()->json($converted);
+        } catch (Exception $err) {
+            return response()->json(['error' => 'An error occurred during create email config. Please try again later.'], 500);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreEmailConfigRequest $request)
+    //get all email config
+    public function getAllEmailConfig(): JsonResponse
     {
-        //
+        try {
+            $emailConfig = EmailConfig::all();
+
+            $converted = arrayKeysToCamelCase($emailConfig->toArray());
+            return response()->json($converted);
+        } catch (Exception $err) {
+            return response()->json(['error' => 'An error occurred during get email config. Please try again later.'], 500);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(EmailConfig $emailConfig)
+    //getSingle email config
+    public function getSingleEmailConfig($id): JsonResponse
     {
-        //
+        try {
+            $emailConfig = EmailConfig::find($id);
+
+            $converted = arrayKeysToCamelCase($emailConfig->toArray());
+            return response()->json($converted);
+        } catch (Exception $err) {
+            return response()->json(['error' => 'An error occurred during get email config. Please try again later.'], 500);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateEmailConfigRequest $request, EmailConfig $emailConfig)
+    //update email config
+    public function updateEmailConfig(Request $request, $id): JsonResponse
     {
-        //
+        try {
+            $emailConfig = EmailConfig::findOrFail($id);
+            $emailConfig->update($request->all());
+
+            if ($emailConfig) {
+                return response()->json(['message' => 'Email config updated successfully.'], 200);
+            } else {
+                return response()->json(['error' => 'Failed to update email config.'], 404);
+            }
+        } catch (Exception $err) {
+            return response()->json(['error' => 'An error occurred during update email config. Please try again later.'], 500);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(EmailConfig $emailConfig)
+    //delete email config
+    public function deleteEmailConfig($id): JsonResponse
     {
-        //
+        try {
+            $emailConfig = EmailConfig::findOrFail($id);
+            $emailConfig->delete();
+
+            if ($emailConfig) {
+                return response()->json(['message' => 'Email config deleted successfully.'], 200);
+            } else {
+                return response()->json(['error' => 'Failed to delete email config.'], 404);
+            }
+        } catch (Exception $err) {
+            return response()->json(['error' => 'An error occurred during delete email config. Please try again later.'], 500);
+        }
     }
 }
