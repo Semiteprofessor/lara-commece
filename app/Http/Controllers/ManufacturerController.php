ef1122ff95ef5b77b78764aee03557339add3d56
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-19 10:29:28 +0100

    Commit

--- app/Http/Controllers/ManufacturerController.php
+++ app/Http/Controllers/ManufacturerController.php
@@ -2,49 +2,86 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Models\Manufacturer;
-use App\Http\Requests\StoreManufacturerRequest;
-use App\Http\Requests\UpdateManufacturerRequest;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class ManufacturerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+
+
+    public function createSingleManufacturer(Request $request): JsonResponse
     {
-        //
+        try{
+
+            $manufacturer = Manufacturer::where('name', $request->name)->first();
+            if($manufacturer){
+                return response()->json(['error' => 'A Manufacturer with the same name already exists'], 400);
+            }
+            $manufacturer = new Manufacturer();
+            $manufacturer->name = $request->name;
+            $manufacturer->save();
+            $converted = arrayKeysToCamelCase($manufacturer->toArray());
+            return response()->json($converted, 201);
+        }catch(Exception $e){
+            return response()->json(['error' => 'An error occurred while creating the Manufacturer record'], 500);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreManufacturerRequest $request)
+
+    public function getAllManufacturer(): JsonResponse
     {
-        //
+        try{
+            $manufacturers = Manufacturer::where('status', 'true')->get();
+            $converted = arrayKeysToCamelCase($manufacturers->toArray());
+            return response()->json($converted, 200);
+        }catch(Exception $e){
+            return response()->json(['error' => 'An error occurred while retrieving the Manufacturers'], 500);
+        }
+    }
+    //get single manufacturer
+    public function getSingleManufacturer($id): JsonResponse
+    {
+        try{
+            $manufacturer = Manufacturer::find($id);
+            if(!$manufacturer){
+                return response()->json(['error' => 'Manufacturer not found'], 404);
+            }
+            $converted = arrayKeysToCamelCase($manufacturer->toArray());
+            return response()->json($converted, 200);
+        }catch(Exception $e){
+            return response()->json(['error' => 'An error occurred while retrieving the Manufacturer'], 500);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Manufacturer $manufacturer)
+
+    public function updateSingleManufacturer(Request $request, $id): JsonResponse
     {
-        //
+        try{
+            $manufacturer = Manufacturer::where('name', $request->name)->first();
+            if($manufacturer){
+                return response()->json(['error' => 'A Manufacturer with the same name already exists'], 400);
+            }
+            $manufacturer = Manufacturer::find($id);
+            $manufacturer->name = $request->name;
+            $manufacturer->save();
+            $converted = arrayKeysToCamelCase($manufacturer->toArray());
+            return response()->json($converted, 200);
+        }catch(Exception $e){
+            return response()->json(['error' => 'An error occurred while updating the Manufacturer record'], 500);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateManufacturerRequest $request, Manufacturer $manufacturer)
+    public function deleteSingleManufacturer(Request $request, $id): JsonResponse
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Manufacturer $manufacturer)
-    {
-        //
+        try{
+            $manufacturer = Manufacturer::find($id);
+            $manufacturer->status = $request->status;
+            $manufacturer->save();
+            return response()->json(['message' => 'Manufacturer record deleted successfully'], 200);
+        } catch(Exception $e){
+            return response()->json(['error' => 'An error occurred while deleting the Manufacturer record'], 500);
+        }
     }
 }
